e2f8962e4cac87ed5e8114808ba08f8157b6363c
Noe Arzate <narzate@gmail.com>
2016-04-27 14:45:21 -0400

    Assignment 10 completed

--- BlocExercises/BlocExercises/EqualityDeterminer.m
+++ BlocExercises/BlocExercises/EqualityDeterminer.m
@@ -12,16 +12,35 @@
 
 - (BOOL) string:(NSString *)string1 isTheSameAsString:(NSString *)string2 {
     /* WORK HERE */
-    return (string1 == string2);
+    //NSString* string1 = @"Hello, there!";
+    //NSString* string2 = = string1;
+    BOOL areTheyTheSame = string1 == string2;
+    NSLog(@"Are they the same? %@", areTheyTheSame ? @"Yes" : @"No");
+    return YES;
+    NSLog(@" Are they not the same? %@", string1 != string2 ? @"yes" : @"No");
+    
+    return NO;
 }
 
 - (BOOL) number:(NSNumber *)number1 isTheSameAsNumber:(NSNumber *)number2 {
     /* WORK HERE */
-    return (number1 == number2);
+    BOOL areTheyEqual = [number1 isEqual:number2];
+    NSLog(@"Are they equal? %@", areTheyEqual ? @"Yes" : @"No");
+    return YES;
+    NSLog(@"Are they not equal? %@", number1 != number2 ? @"Yes" : @"No");
+    return NO;
+    
+    
+    
 }
 
 - (BOOL) integer:(NSInteger)integer1 isGreaterThan:(NSInteger)integer2 {
     /* WORK HERE */
+   // NSLog(@"Is integer1 greater than integer2? %@", integer1 > integer2 ? @"Yes" : @"No");
+    
+    //return YES;
+    NSLog(@"Is integer1 greater than integer2? %@", integer1 > integer2 ? @"Yes" : @"No");
+    
     return NO;
 }
 
