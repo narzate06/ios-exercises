8951a85941bd78d63deb48e8af35e8381949ea87
Noe Arzate <narzate@gmail.com>
2016-05-05 15:12:22 -0500

    Assignment 11 ifelse

--- BlocExercises/BlocExercises/MarysAppleHandler.m
+++ BlocExercises/BlocExercises/MarysAppleHandler.m
@@ -14,6 +14,17 @@
     NSString *itemToReturn;
 
     /* WORK HERE */
+    if (dollars == 4) {
+        itemToReturn = @"get out of my store";
+    }else if (dollars == 5){
+        itemToReturn = @"have some gum";
+    }else if (dollars == 6){
+        itemToReturn = @"have an apple";
+    }else if (dollars == 1000){
+        itemToReturn = @"have an Apple computer";
+    }else if (dollars == 1000000000){
+        itemToReturn = @"have The Big Apple";
+    }
     
     NSLog(@"For $%ld, Mary can: %@", (long)dollars, itemToReturn);
     return itemToReturn;
@@ -24,9 +35,10 @@
 
     NSUInteger cost = 24;
     
-    if (self.getsDiscount) {
-        cost *= .75;
-    }
+   // if (self.getsDiscount) {
+   //     cost *= .75;
+   // }
+    cost = (self.getsDiscount ? 18 : 24);
     
     return cost;
 }
